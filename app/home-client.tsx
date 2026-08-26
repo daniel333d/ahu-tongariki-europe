eeff3c224bd95dea755a95d9fb56ae6f9e7de630
daniel333d <nowicki.d@hotmail.com>
2026-08-26 22:48:52 +0200

    Restore promotional video poster

--- app/home-client.tsx
+++ app/home-client.tsx
@@ -1001,7 +1001,7 @@ function HomeContent() {
           <CinematicVideoSection
             caption={copy.location.video.caption}
             videoSrc="/assets/video/ahu-tongariki-europe-film.mp4"
-            posterSrc="/ahu-tongariki-hero-optimized.webp"
+            posterSrc="/assets/video/ahu-tongariki-europe-poster.webp"
             playAriaLabel={copy.location.video.playAriaLabel}
             posterHeading={copy.location.video.posterHeading}
             playLabel={copy.location.video.playLabel}
